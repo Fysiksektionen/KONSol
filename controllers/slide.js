2e5a75e889775899e6c00cf1a76542888562deb8
Acksell <axelen@kth.se>
2018-06-14 00:07:13 +0200

    Extracted errorHandler.

--- controllers/slide.js
+++ controllers/slide.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Slide = require('../models/slide')
 const errorChecks = require('../errors/error-checks.js')
+const errorHandlers = require('../errors/errorHandlers.js')
 
 exports.getAllSlides = function(req, res) {
     // Return all slides
@@ -12,23 +13,7 @@ exports.create = function(req, res) {
     if (!req.body) return res.sendStatus(400)
     Slide.create(req.body)
         .then(slide => {res.status(201);res.json({"ok":true, slide})})
-        .catch(err => {
-            if (err instanceof mongoose.Error.ValidationError){
-                res.status(400)
-                res.json({'message':'Validation failed',status:400,ok:false,errorName:'ValidationError',errors:err.errors})
-            }
-            else if (errorChecks.DuplicateError) {
-                res.status(400)
-                res.json({'message':err.message,status:400,ok:false,errorName:'DuplicateKeyError',errors:err.errors})
-            }
-            else {
-                // Can we catch any other known errors here? Either way we should log it.
-                // This is the downside to not using an error library.
-                // log(err.message, err.errors)
-                console.log(err)
-                res.sendStatus(500)
-            }
-        })
+        .catch(errorHandlers.CreationError(req, res))
 }
 
 exports.getById = function(req, res) {
@@ -38,12 +23,13 @@ exports.getById = function(req, res) {
 // Currently a POST without a body. Could make the id a required part of the request body
 // and simply make the route [...]/remove.
 exports.removeById = function(req, res) {
-    // Status is {"ok":1, "n":1} if all went well. Can return status.n = 0.
+    // Status is {"ok":1, "n":1} if all went as expected. 
+    // Can return {"ok":1, "n":0}, meaning id was technically valid but no object had it.
     if (mongoose.Types.ObjectId.isValid(req.params.id)) {
         Slide.deleteOne({_id:req.params.id}).then(status => res.json(status))
     }
     else {
         res.status(400)
-        res.json({'message':'InvalidId', status: 400, ok: false,errorName:'InvalidIdError'})
+        res.json({'message':'Invalid id', status: 400, ok: false, errorName: 'InvalidIdError'})
     }
 }
---
+++ errors/errorHandlers.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const errorChecks = require('./error-checks.js')
+
+exports.CreationError = (req, res) => err => {
+    if (err instanceof mongoose.Error.ValidationError){
+        res.status(400)
+        res.json({'message':'Validation failed',status:400,ok:false,errorName:'ValidationError',errors:err.errors})
+    }
+    else if (errorChecks.DuplicateError(err)) {
+        res.status(400)
+        res.json({'message':err.message,status:400,ok:false,errorName:'DuplicateKeyError',errors:err.errors})
+    }
+    else {
+        // TODO better logging.
+        // Can we catch any other known errors here? Either way we should log it with a real logger.
+        // This is the downside to not using an error library.
+        // log(err.message, err.errors)
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
